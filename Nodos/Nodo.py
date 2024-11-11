4c3d51332d48a739a2788d1ad265b1aed41682a6
Carlos161104 <canoja161104@mail.com>
2024-11-11 11:58:25 -0600

    feat: update Nodo.py, resive the frames array from cluster

--- Nodos/Nodo.py
+++ Nodos/Nodo.py
@@ -11,7 +11,52 @@
     ### Se envia el segmento
 ## 6. Se setean las variables de datos para despues regresar al modo servidor en espera de mas segmentos de video.
 from socket import *
+import cv2
 
+def recibir_frames(conn, frames):
+    ## Función que maneja la conexión con un cliente.
+    ## Recibe: Frames de video desde el cliente.
+    ## Retorna: Lista de frames descodificados.
+    print("Conectado al servidor")
+    while True:
+        try:
+            frame_size = conn.recv(4)
+            if not frame_size:
+                break
+            frame_size = int.from_bytes(frame_size, byteorder='big')
+            frame_data = b''
+            while len(frame_data) < frame_size:
+                packet = conn.recv(frame_size - len(frame_data))
+                if not packet:
+                    return None
+                frame_data += packet
+            frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
+            frames.append(frame)
+        except Exception as e:
+            print(f"Error recibiendo frame: {e}")
+            break
+    return frames
+
+def Recive_Video(frames):
+    host = socket.gethostname(socket.gethostname())
+    buff = 1024
+    port = 5000
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    
+    print(f"Esperando el video en la conexion {host}:{port}...")
+
+    
+    while True: 
+        conn, addr = server_socket.accept()
+        print("Cluster enviando video...")
+        
+        frames = recibir_frames(conn, frames)
+        break;
+    server_socket.close()
+    return frames  
 
 def Main():
     host= "localhost"
@@ -24,3 +69,10 @@ def Main():
     #Enviar el tipo 1 para el server sepa que soy un cliente
     mySocket.send('2'.encode())
     print("Enviando el tipo ...")
+    frames = []
+    
+    frames = Recive_Video()
+    
+    #En este punto ua tenemos el arreglo de frames.
+    
+    
